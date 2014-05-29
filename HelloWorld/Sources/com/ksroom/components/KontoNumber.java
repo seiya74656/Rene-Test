c5e3276030c3f93befd0cffac74608ced2ddd17d
Ken ISHIMOTO <ken@ksroom.com>
2014-05-29 17:33:21 +0200

    change to full serial
    using Dash will help

--- HelloWorld/Sources/com/ksroom/components/KontoNumber.java
+++ HelloWorld/Sources/com/ksroom/components/KontoNumber.java
@@ -5,7 +5,15 @@ import com.webobjects.appserver.WOContext;
 import er.extensions.components.ERXComponent;
 
 public class KontoNumber extends ERXComponent {
-	  private static final long serialVersionUID = 1L;
+  /**
+   * Do I need to update serialVersionUID?
+   * See section 5.6 <cite>Type Changes Affecting Serialization</cite> on page 51 of the 
+   * <a href="http://java.sun.com/j2se/1.4/pdf/serial-spec.pdf">Java Object Serialization Spec</a>
+   */
+  private static final long serialVersionUID = 1L;
+
+  
+  
 
 	  
   public KontoNumber(WOContext aContext) {
